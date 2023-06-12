b574604aef2d31848aa21c5b08aaa148d2444e62
SebSaurin <124032228+SebSaurin@users.noreply.github.com>
2023-06-12 15:36:11 +0200

    Update load_data.py
    New datasets

--- XPER/datasets/load_data.py
+++ XPER/datasets/load_data.py
@@ -2,8 +2,16 @@ import os
 from os.path import join
 import pandas as pd
 import numpy as np
+import sklearn.datasets
 
 def boston():
     df = pd.read_csv("BostonHousing.csv")
     return df
 
+def iris():
+    data = sklearn.datasets.load_iris()
+    return data
+
+def adult():
+    data =  pd.read_csv("adult.data", sep=",")
+    return data
