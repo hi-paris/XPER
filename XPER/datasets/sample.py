d2b3394ab4ffa41393a9c0cfe34b51e751cbb423
SebSaurin <124032228+SebSaurin@users.noreply.github.com>
2023-06-19 18:39:14 +0200

    Update sample.py
    Add new parameters

--- XPER/datasets/sample.py
+++ XPER/datasets/sample.py
@@ -4,7 +4,7 @@ import numpy as np
 from sklearn.metrics import roc_auc_score
 from scipy.stats import norm
 import statsmodels.api as sm
-def sample_generation(N=500, p=6, seed=123456):
+def sample_generation(N=500, p=6, seed=123456, pct_train=0.7, mean=np.repeat(0, p), cov=np.repeat(1.5,p)*np.eye(p), beta=np.repeat(0.5,p), beta_0=0.75):
     """
     Generate a synthetic dataset for binary classification using a multivariate normal distribution.
 
@@ -12,7 +12,15 @@ def sample_generation(N=500, p=6, seed=123456):
         N (int): Size of the sample (default: 500)
         p (int): Number of explanatory variables (default: 6)
         seed (int): Random seed for reproducibility (default: 123456)
-
+        pct_train (float): Percentage of the dataset devoted to the training of the algorithm (default: 70%)
+        mean (ndarray): Vector of size p containing the expected value of the variables (default: np.array([0, 0, ..., 0, 0]))
+        cov (ndarray): Covariance matrix of size p x p  (default: array([[1.5, 0. , 0. , 0. , 0. , 0. ],
+                                                                         [0. , 1.5, 0. , 0. , 0. , 0. ],
+                                                                         [...],
+                                                                         [0. , 0. , 0. , 0. , 0. , 1.5]]) )
+        beta (ndarray): Vector of parameters of size p (default: np.array([0.5, 0.5, ..., 0.5, 0.5])
+        beta_0 (float): Intercept value (default: 0.75)
+        
     Returns:
         X_train (ndarray): Training set features
         y_train (ndarray): Training set labels
@@ -26,26 +34,10 @@ def sample_generation(N=500, p=6, seed=123456):
     # Set the random seed for reproducibility
     random.seed(seed)
     np.random.seed(seed)
-
-    # Set the sample size and number of variables
-    N = 500  # Size of the sample
-    p = 6    # Number of explanatory variables
-
-    # Generate a multivariate normal distribution
-    mean = np.repeat(0, p)  # Mean equal to 0 for all variables
-    var = np.array([1.5, 1.4, 1.3, 1.2, 1.1, 1])  # Variances for each variable
-    cov = var * np.eye(p)  # Covariance matrix (diagonal)
-
+    
     X = np.random.multivariate_normal(mean, cov, N)
-    print(X)
 
     # Simulate the model
-
-    # Coefficients
-    beta = np.array([0.5, 0.5, 0.5, 0.5, 0.5, 0.5]).T
-    print("Number of coefficients (including the intercept):", np.shape(beta)[0])
-
-    beta_0 = 0.75  # Intercept
 
     # Errors
     mu_error, std_error = 0, 1
@@ -59,14 +51,9 @@ def sample_generation(N=500, p=6, seed=123456):
     y[y > 0] = 1
     y[y <= 0] = 0
 
-    # Calculate the proportion of positive labels
-    print("Calculate the proportion of positive labels: ",np.sum(y == 1) / len(y))
-
-    # Calculate the probabilities
-    proba = norm.cdf(index)
-
     # Train/Test split
-    N_train = int(0.7 * N)  # 70% of the sample for training
+    
+    N_train = int(pct_train* N)  # 70% of the sample for training
 
     X_train = X[:N_train, :]
     y_train = y[:N_train]
@@ -75,5 +62,3 @@ def sample_generation(N=500, p=6, seed=123456):
     y_test = y[N_train:]
 
     return X_train, y_train, X_test, y_test, p, N, seed
-
-#X_train, y_train, X_test, y_test, p, N, seed  = sample_generation(N=500,p=6,seed=123456)
