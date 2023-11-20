03c52352fd20b7038850fb49b2493f5599b9d1bb
Gaëtan <gaetan.brison@gmail.com>
2023-11-21 00:07:23 +0100

    setup

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/hi-paris/XPER',
-    version='0.0.73',
+    version='0.0.74',
     zip_safe=False,
 )
