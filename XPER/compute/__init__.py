0d03247fa2071bdd24954975e2296765d79fceaa
SebSaurin <124032228+SebSaurin@users.noreply.github.com>
2024-06-04 15:34:34 +0200

    Delete the code trying to load a deleted file

--- XPER/compute/__init__.py
+++ XPER/compute/__init__.py
@@ -1,6 +1,5 @@
 #from XPER.compute import * 
 from XPER.compute.EM import * 
 from XPER.compute.Optimisation import OptimizationClass
-from XPER.compute.XGBoost_model import *
 from XPER.compute.Performance import *
 #from XPER.compute.Load import xgboost
